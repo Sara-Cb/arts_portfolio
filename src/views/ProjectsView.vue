4df5ea65120924b16964ae4c3d7af3fa2f6b6ef0
Sara-Cb <123389945+Sara-Cb@users.noreply.github.com>
2025-02-23 17:25:44 +0100

    added projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,7 +1,27 @@
 <script setup>
+import { useCategoriesStore } from '@/stores/categories';
+import { storeToRefs } from 'pinia';
+import { RouterLink } from 'vue-router';
+
+const store = useCategoriesStore();
+const { categories } = storeToRefs(store);
+
+
 
 </script>
 
 <template>
+    <div class="pageContainer">
+
+        <div class="projectsMenu">
+            <h1 class="title">Projects</h1>
+            <div class="categories">
+                <RouterLink v-for="cat in categories" :to="'/projects/' + cat.type" :class="'category' + cat.type"
+                    :key="cat.type">
+                    <p>{{ cat.title }}</p>
+                </RouterLink>
+            </div>
+        </div>
+    </div>
 
 </template>

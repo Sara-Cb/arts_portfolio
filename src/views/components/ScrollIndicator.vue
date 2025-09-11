b64ab2ac542940c849f5b518b55ba362bfcdc670
Sara.cb <123389945+Sara-Cb@users.noreply.github.com>
2025-09-11 12:42:57 +0200

    fix: scroll indicator are more comfy now

--- src/views/components/ScrollIndicator.vue
+++ src/views/components/ScrollIndicator.vue
@@ -26,45 +26,52 @@ function goToSection(sectionName) {
       class="dot"
       :class="{ active: index === activeIndex }"
       @click="goToSection(section)"
-    ></div>
+    ><div class="dot-inner"></div></div>
   </div>
 </template>
 
 <style scoped>
 .scrollbar {
-  position: fixed;
-  top: 50%;
-  right: 0.7rem;
-  transform: translateY(-50%);
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  z-index: 500;
+    position: fixed;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.3rem;
+    z-index: 500;
 }
 
 @media screen and (min-width: 576px) {
   .scrollbar {
-    right: 1rem;
+    right: 0.5rem;
   }
 }
 
 .dot {
-  width: 5px;
-  height: 16px;
+  width: 15px;
+  height: 26px;
+  padding: 5px;
+}
+
+.dot-inner {
+  height: 100%;
+  width: 100%;
   background-color: #f02020;
   border-radius: 2px;
   opacity: 0.3;
   transition: opacity 0.3s, background-color 0.3s;
 }
 
-.dot.active {
+.dot.active .dot-inner{
   opacity: 1;
   background-color: #6e0000;
 }
 
 @media (hover: hover) {
-  .dot:hover {
-    opacity: 0.5;
+  .dot:hover .dot-inner {
+    opacity: 0.7;
   }
 }
 </style>
